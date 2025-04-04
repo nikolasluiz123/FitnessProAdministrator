a27d905dee6e2d9b313f24510f4b4662733dcc3e
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-04-03 21:11:36 -0300

    feat: melhoria na conversão de datas e hora
    Deixando explíssito o locale usado

--- src/main/java/br/com/administrator/utils/TimeConverterUtil.java
+++ src/main/java/br/com/administrator/utils/TimeConverterUtil.java
@@ -3,6 +3,8 @@ package br.com.administrator.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Locale.Category;
 
 import br.com.administrator.enums.EnumDateTimePatterns;
 
@@ -11,28 +13,32 @@ public class TimeConverterUtil {
 	public static LocalDateTime parseLocalDateTime(String value, EnumDateTimePatterns pattern) {
 		if (StringUtils.isNullOrEmpty(value)) return null;
 				
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern.getPattern());
+		DateTimeFormatter formatter = getFormatterOfPatternWithLocale(pattern);
 		return LocalDateTime.parse(value, formatter);
 	}
 	
 	public static LocalDate parseLocalDate(String value, EnumDateTimePatterns pattern) {
 		if (StringUtils.isNullOrEmpty(value)) return null;
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern.getPattern());
+		DateTimeFormatter formatter = getFormatterOfPatternWithLocale(pattern);
 		return LocalDate.parse(value, formatter);
 	}
-	
+
 	public static String formatLocalDateTime(LocalDateTime value, EnumDateTimePatterns pattern) {
 		if (value == null) return null;
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern.getPattern());
+		DateTimeFormatter formatter = getFormatterOfPatternWithLocale(pattern);
 		return formatter.format(value);
 	}
 	
 	public static String formatLocalDate(LocalDate value, EnumDateTimePatterns pattern) {
 		if (value == null) return null;
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern.getPattern());
+		DateTimeFormatter formatter = getFormatterOfPatternWithLocale(pattern);
 		return formatter.format(value);
+	}
+	
+	private static DateTimeFormatter getFormatterOfPatternWithLocale(EnumDateTimePatterns pattern) {
+		return DateTimeFormatter.ofPattern(pattern.getPattern(), Locale.getDefault(Category.FORMAT));
 	}
 }
