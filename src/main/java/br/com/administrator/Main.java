3543b87352ffca860e881c4fd8316a5cccebdc1a
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-10-16 20:52:29 -0300

    fix: ajuste no main

--- src/main/java/br/com/administrator/Main.java
+++ src/main/java/br/com/administrator/Main.java
@@ -1,12 +1,15 @@
 package br.com.administrator;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.JarResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 import org.apache.tomcat.util.scan.StandardJarScanner;
 
 import java.io.File;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -32,8 +35,26 @@ public class Main {
             context.setJarScanner(jarScanner);
         } else {
             System.out.println("Configurando para ambiente de produção (JAR)...");
-            Context context = tomcat.addWebapp(contextPath, "/");
-            context.addLifecycleListener(new Tomcat.FixContextListener());
+            // Cria um contexto vazio. O docBase é nulo porque vamos definir os recursos manualmente.
+            Context context = tomcat.addContext(contextPath, null);
+
+            // Descobre a localização do JAR que está em execução
+            final URL location = Main.class.getProtectionDomain().getCodeSource().getLocation();
+            final String docBase = location.toURI().getPath();
+
+            // Aponta o gerenciador de recursos para o próprio JAR
+            WebResourceRoot resources = new StandardRoot(context);
+            // O caminho webAppMount é a raiz da aplicação, e o internalPath é onde os recursos
+            // estão dentro do JAR (normalmente em META-INF/resources)
+            resources.addPreResources(new JarResourceSet(resources, "/", docBase, "/META-INF/resources"));
+            context.setResources(resources);
+
+            // Como estamos configurando manualmente, precisamos registrar o FacesServlet
+            Tomcat.addServlet(context, "FacesServlet", "jakarta.faces.webapp.FacesServlet");
+            context.addServletMappingDecoded("*.jsf", "FacesServlet");
+
+            // Também é bom adicionar o listener do Weld (CDI) aqui
+            context.addApplicationListener("org.jboss.weld.environment.servlet.Listener");
         }
 
         tomcat.start();
