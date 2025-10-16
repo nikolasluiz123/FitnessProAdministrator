e3f8eaf5c76de9fd87886e2df3179950f8469d9f
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-10-16 19:57:11 -0300

    fix: ajuste para inicializar corretamente o server

--- src/main/java/br/com/administrator/Main.java
+++ src/main/java/br/com/administrator/Main.java
@@ -2,7 +2,10 @@ package br.com.administrator;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+
 import java.io.File;
+import java.nio.file.Files;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -10,8 +13,15 @@ public class Main {
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        Context context = tomcat.addWebapp("", new File(".").getAbsolutePath());
-        Tomcat.initWebappDefaults(context);
+        File baseDir = Files.createTempDirectory("tomcat-base").toFile();
+        baseDir.deleteOnExit();
+        Context context = tomcat.addContext("", baseDir.getAbsolutePath());
+
+        context.addLifecycleListener(new Tomcat.DefaultWebXmlListener());
+
+        StandardJarScanner jarScanner = new StandardJarScanner();
+        jarScanner.setScanClassPath(true);
+        context.setJarScanner(jarScanner);
 
         tomcat.start();
         tomcat.getServer().await();
