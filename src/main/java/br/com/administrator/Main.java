e3d3fc1788dcded6c0f4ae83b34b1443a5db3411
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-10-16 19:23:21 -0300

    fix: ajuste nos paths setados no tomcat

--- src/main/java/br/com/administrator/Main.java
+++ src/main/java/br/com/administrator/Main.java
@@ -1,9 +1,6 @@
 package br.com.administrator;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 
@@ -13,14 +10,7 @@ public class Main {
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        String webappDirLocation = "src/main/webapp/";
-        Context context = tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
-
-        File additionWebInfClasses = new File("target/classes");
-
-        StandardRoot resources = new StandardRoot(context);
-        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
-        context.setResources(resources);
+        tomcat.addWebapp("", new File(".").getAbsolutePath());
 
         tomcat.start();
         tomcat.getServer().await();
